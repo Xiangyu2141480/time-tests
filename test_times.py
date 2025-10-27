c4e870d99c51bc09c2a46d1c40406ae157f93a5f
Xiangyu Cui <ucapxcu@ucl.ac.uk>
2025-10-27 22:39:49 +0000

    Create three further tests

--- test_times.py
+++ test_times.py
@@ -11,3 +11,35 @@ def test_times():
     ]
 
     assert result == expected
+
+def test_no_overlap():
+    range1 = time_range('2010-01-12 10:00:00', '2010-01-12 13:00:00')
+    range2 = time_range('2010-01-12 14:00:00', '2010-01-12 15:00:00')
+    result = compute_overlap_time(range1, range2)
+
+    expected = []
+
+    assert result == expected
+
+
+def test_multiple_intervals_overlap():
+    range1 = time_range("2010-01-12 10:00:00", "2010-01-12 11:00:00", 2)
+    range2 = time_range("2010-01-12 10:15:00", "2010-01-12 11:15:00", 2)
+    result = compute_overlap_time(range1, range2)
+
+    expected = [
+        ('2010-01-12 10:15:00', '2010-01-12 10:30:00'),
+        ('2010-01-12 10:30:00', '2010-01-12 10:45:00'),
+        ('2010-01-12 10:45:00', '2010-01-12 11:00:00')
+    ]
+
+    assert result == expected
+
+def test_touching_overlap():
+    range1 = time_range("2010-01-12 10:00:00", "2010-01-12 11:00:00")
+    range2 = time_range("2010-01-12 11:00:00", "2010-01-12 11:15:00")
+    result = compute_overlap_time(range1, range2)
+
+    expected = []
+
+    assert result == expected
